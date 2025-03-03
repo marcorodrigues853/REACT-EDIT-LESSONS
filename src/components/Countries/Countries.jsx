0030edab72545e7458d4964439dacd5926b00443
Marco Rodrigues <marcoaurelio853@gmail.com>
2025-03-03 22:17:30 +0000

    new updates

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import CountriesList from '../../aula-3/CountryList';
+
 import './Countries.css';
+import CountryCard from './CountryCard';
 function Countries() {
     const [listaPaises, setListaPaises] = useState([
         {
@@ -20,29 +21,69 @@ function Countries() {
             capital: 'Habana',
         },
         {
-            name: 'Spain',
+            name: 'Espanha',
             moeda: 'eur',
             idioma: 'espanol',
             numberOfHabitants: 51_000_000,
             bandeira: 'https://flagcdn.com/w320/es.png',
             capital: 'Madrid',
         },
+        {
+            name: 'Espanhalovaquia',
+            moeda: 'eur',
+            idioma: 'espanol',
+            numberOfHabitants: 51_000_000,
+            bandeira: 'https://flagcdn.com/w320/sl.png',
+            capital: 'QUALQERT COISA',
+        },
     ]);
 
-    const usaCountry = {
-        name: 'Usa',
-        moeda: 'usd',
-        idioma: 'English',
-        numberOfHabitants: 51_000_000,
-        bandeira: 'https://flagcdn.com/w320/us.png',
+    const [search, setSearch] = useState('');
+    const [form, setForm] = useState({
+        name: '',
+        moeda: '',
+        idioma: '',
+        numberOfHabitants: '',
+        bandeira: '',
         capital: '',
+    });
+
+    // USetate spor cada campo
+
+    function handleChange(event) {
+        console.log('event: ' + event.target.name + ': ' + event.target.value);
+
+        const keyName = event.target.name;
+        const value = event.target.value;
+        setForm({
+            ...form,
+            // key (nome, ): value
+            [keyName]: value,
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setListaPaises([
+            ...listaPaises,
+            { ...form, numberOfHabitants: Number(form.numberOfHabitants) },
+        ]);
+        setForm({
+            name: '',
+            moeda: '',
+            idioma: '',
+            numberOfHabitants: '',
+            bandeira: '',
+            capital: '',
+        });
     };
 
-    console.log('listaPaises', listaPaises);
     const [labelBotao, setLabelBotao] = useState('clicar aqui!!!');
     const [cor, setCor] = useState('royalblue');
     const [isDark, setIsDark] = useState(true);
 
+    console.log('listaPaise', listaPaises);
+    console.log('search', search);
     return (
         <div
             className="container"
@@ -50,26 +91,30 @@ function Countries() {
             <h1 className="title">🌍 Lista de Países</h1>
 
             {/* Campo de Pesquisa */}
-            <input
-                type="text"
-                placeholder="Buscar país..."
-                className="search-input"
-                value={'ola'}
-            />
-
+            <div className="flex">
+                <input
+                    type="text"
+                    placeholder="Search país..."
+                    className="search-input"
+                    value={search}
+                    onChange={(event) => {
+                        setSearch(event.target.value);
+                    }}
+                />
+                <button>SEARCH</button>
+            </div>
             {/* GRELHA */}
             <div className="grid">
-                {listaPaises.map((pais, index) => (
-                    <div className="card" key={index}>
-                        <img className="flag" src={pais.bandeira} />
-                        <h2>{pais.name}</h2>
-                        <p>{pais.capital}</p>
-                        <p>
-                            <strong>IDIOMA:</strong> {pais.idioma}
-                        </p>
-                        <p>{pais.numberOfHabitants}</p>
-                    </div>
-                ))}
+                {listaPaises
+                    // .filter((pais) => "Portugal" === "Portu")
+                    .filter((pais) =>
+                        pais.name
+                            .toLocaleLowerCase()
+                            .startsWith(search.toLocaleLowerCase())
+                    )
+                    .map((pais, index) => (
+                        <CountryCard pais={pais} key={index} />
+                    ))}
             </div>
 
             <div className="flex">
@@ -87,7 +132,7 @@ function Countries() {
                     is dark ? {isDark ? 'sim' : 'nao'}
                 </button>
                 <button
-                    onClick={() =>
+                    onClick={() => {
                         setListaPaises((prev) => [
                             ...prev,
                             {
@@ -98,11 +143,79 @@ function Countries() {
                                 bandeira: 'https://flagcdn.com/w320/us.png',
                                 capital: 'Washinton',
                             },
-                        ])
-                    }>
+                        ]);
+                    }}>
                     adiconar USA
                 </button>
+                <button
+                    onClick={() =>
+                        setListaPaises((prev) => [
+                            ...prev,
+                            {
+                                name: 'Italy',
+                                moeda: 'usd',
+                                idioma: 'Italian',
+                                numberOfHabitants: 51_000_000,
+                                bandeira: 'https://flagcdn.com/w320/it.png',
+                                capital: 'Milan',
+                            },
+                        ])
+                    }>
+                    adiconar ITALY
+                </button>
             </div>
+
+            <form onSubmit={handleSubmit} className="container form ">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Country Name"
+                    value={form.name}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="moeda"
+                    placeholder="Currency"
+                    value={form.moeda}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="idioma"
+                    placeholder="Language"
+                    value={form.idioma}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="number"
+                    name="numberOfHabitants"
+                    placeholder="Population"
+                    value={form.numberOfHabitants}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="bandeira"
+                    placeholder="Flag URL"
+                    value={form.bandeira}
+                    onChange={handleChange}
+                    required
+                />
+                <input
+                    type="text"
+                    name="capital"
+                    placeholder="Capital"
+                    value={form.capital}
+                    onChange={handleChange}
+                    required
+                />
+                <button type="submit">Add Country</button>
+            </form>
         </div>
     );
 }
